2541d36afae35c773d7d90c27cbded8fa683c965
Andrew <49104171+carsonak@users.noreply.github.com>
2023-12-14 13:15:06 +0300

    Added monty.h

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 0x19. C - Stacks, Queues - LIFO, FIFO
+
+compile command: gcc -Wall -Werror -Wextra -pedantic -std=c89 *.c -o monty
---
+++ hello.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Hello Monty!\n");
+
+	return (0);
+}
