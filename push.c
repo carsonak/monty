23027958c6cb93457f0d2527acd571774d833938
Andrew <49104171+carsonak@users.noreply.github.com>
2023-12-15 22:29:24 +0300

    Restructured push

--- push.c
+++ push.c
@@ -14,10 +14,9 @@ void push(stack_t **stack, unsigned int line_number)
 	if (!carry_var.arg)
 		clean_exit(*stack, "push_no_int", NULL, line_number);
 
+	alt = carry_var.arg;
 	if (carry_var.arg[0] == '-')
-		alt = carry_var.arg + 1;
-	else
-		alt = carry_var.arg;
+		alt++;
 
 	for (i = 0; alt[i]; i++)
 		if (!isdigit(alt[i]))
@@ -26,20 +25,17 @@ void push(stack_t **stack, unsigned int line_number)
 	if (stack)
 		temp = malloc(sizeof(stack_t));
 
-	if (temp)
-	{
-		if (carry_var.arg[0] == '-')
-			temp->n = -atoi(alt);
-		else
-			temp->n = atoi(alt);
-
-		temp->next = *stack;
-		temp->prev = NULL;
-		if (*stack)
-			(*stack)->prev = temp;
-
-		*stack = temp;
-	}
-	else
+	if (!temp)
 		clean_exit(*stack, "malloc", NULL, line_number);
+
+	temp->n = atoi(alt);
+	if (carry_var.arg[0] == '-')
+		temp->n = -temp->n;
+
+	temp->next = *stack;
+	temp->prev = NULL;
+	if (*stack)
+		(*stack)->prev = temp;
+
+	*stack = temp;
 }
