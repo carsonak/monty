60675d70fd97b7ff9b79b24b979698b21a03d628
Andrew <49104171+carsonak@users.noreply.github.com>
2025-04-13 13:27:27 +0300

    Turned on formatting warnings

--- print_error.c
+++ print_error.c
@@ -15,31 +15,31 @@ void print_error(
 	switch (err)
 	{
 	case DIVISION_BY_ZERO:
-		fprintf(stderr, "L%ld: division by zero\n", line_num);
+		fprintf(stderr, "L%u: division by zero\n", line_num);
 		break;
 	case EMPTY_STACK:
-		fprintf(stderr, "L%ld: can't %s, stack empty\n", line_num, opcode);
+		fprintf(stderr, "L%u: can't %s, stack empty\n", line_num, opcode);
 		break;
 	case INVALID_ARGS:
 		fprintf(stderr, "USAGE: monty file\n");
 		break;
 	case UNKNOWN_OPCODE:
-		fprintf(stderr, "L%ld: unknown instruction %s\n", line_num, opcode);
+		fprintf(stderr, "L%u: unknown instruction %s\n", line_num, opcode);
 		break;
 	case MALLOC_FAIL:
 		fprintf(stderr, "Error: malloc failed\n");
 		break;
 	case PCHAR_NOT_ASCII:
-		fprintf(stderr, "L%ld: can't pchar, value out of range\n", line_num);
+		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_num);
 		break;
 	case POP_EMPTY_STACK:
-		fprintf(stderr, "L%ld: can't pop an empty stack\n", line_num);
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_num);
 		break;
 	case PUSH_NO_INTEGER:
-		fprintf(stderr, "L%ld: usage: push integer\n", line_num);
+		fprintf(stderr, "L%u: usage: push integer\n", line_num);
 		break;
 	case STACK_TOO_SHORT:
-		fprintf(stderr, "L%ld: can't %s, stack too short\n", line_num, opcode);
+		fprintf(stderr, "L%u: can't %s, stack too short\n", line_num, opcode);
 		break;
 	default:
 		break;
